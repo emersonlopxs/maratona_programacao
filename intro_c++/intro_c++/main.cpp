a2351646bf049863d36cee3f7a4d861d8e33400d
lupuselit <lupuselit@outlook.com>
2019-05-09 21:55:58 -0300

    add strings

--- intro_c++/intro_c++/main.cpp
+++ intro_c++/intro_c++/main.cpp
@@ -10,7 +10,42 @@
 using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    cout << "Hello, Worlddddd!\n";
+    /*
+    int num;
+    cout << "please enter a number: ";
+    cin >> num;
+    cout << num << endl << "dudeee\n";
+    char name[101];
+     */
+    
+    /*
+    string name;
+    int number = 2;
+    char lastName[101] =  "Lopes";
+    double numDecimal = 3.2;
+    float decimal = 2.2;
+    bool isMale = true;
+
+    cout << "please enter your name: ";
+    cin >> name;
+    cout << "your name is " << name << endl;
     return 0;
+     */
+    
+    /*
+    string phrase = "emerson lopes";
+    
+    cout << phrase.length() << endl;
+    cout << phrase[2] << endl;
+    cout << phrase.size() << endl;
+    
+//    returns the first index found
+    cout << phrase.find("e") << endl;
+//    start index, lenght
+    cout << phrase.substr(8, 3) << endl;
+     */
+    
+    
+    
+    
 }
