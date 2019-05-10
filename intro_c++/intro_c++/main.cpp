94ec05081f0010607443c93e2cb081ab704c39c3
lupuselit <lupuselit@outlook.com>
2019-05-09 21:36:24 -0300

    first commit

---
+++ intro_c++/intro_c++/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  intro_c++
+//
+//  Created by Emerson Lopes on 09/05/19.
+//  Copyright © 2019 Emerson Lopes. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    cout << "Hello, Worlddddd!\n";
+    return 0;
+}
